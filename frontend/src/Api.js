bbc1ff6b47c5ba358901abac6ab99c24ad57840e
Victor Breder <vbbreder@gmail.com>
2018-06-13 23:12:25 -0300

    option to disable api logs

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -6,48 +6,70 @@ export default class Api {};
 
 // This is a wrapper around the Axios library so we can log all Api calls and responses.
 
-Api.get = (route) => {
-  return new Promise((resolve, reject) => {
-    Axios.get(Config.api + route).then((res) => {
-      console.log("API GET", route, res.data);
-      resolve(res);
-    }).catch((err) => {
-      console.log("ERROR: API GET", route, err);
-      reject(err);
-    });
-  });
-}
+if (Config.enableApiLogs) {
 
-Api.post = (route, params) => {
-  return new Promise((resolve, reject) => {
-    Axios.post(Config.api + route, params).then((res) => {
-      console.log("API POST", route, params, res.data);
-      resolve(res);
-    }).catch((err) => {
-      console.log("ERROR: API POST", route, params, err);
-      reject(err);
+  Api.get = (route) => {
+    return new Promise((resolve, reject) => {
+      Axios.get(Config.api + route).then((res) => {
+        console.log("API GET", route, res.data);
+        resolve(res);
+      }).catch((err) => {
+        console.log("ERROR: API GET", route, err);
+        reject(err);
+      });
     });
-  });
-}
+  }
 
-Api.put = (route, params) => {
-  return new Promise((resolve, reject) => {
-    Axios.put(Config.api + route, params).then((res) => {
-      console.log("API PUT", route, params, res.data);
-      resolve(res);
-    }).catch((err) => {
-      console.log("ERROR: API PUT", route, params, err);
+  Api.post = (route, params) => {
+    return new Promise((resolve, reject) => {
+      Axios.post(Config.api + route, params).then((res) => {
+        console.log("API POST", route, params, res.data);
+        resolve(res);
+      }).catch((err) => {
+        console.log("ERROR: API POST", route, params, err);
+        reject(err);
+      });
     });
-  });
-}
+  }
 
-Api.delete = (route, params) => {
-  return new Promise((resolve, reject) => {
-    Axios.delete(Config.api + route, params).then((res) => {
-      console.log("API DELETE", route, params, res.data);
-    }).catch((err) => {
-      console.log("ERROR: API DELETE", route, params, err);
-      reject(err);
+  Api.put = (route, params) => {
+    return new Promise((resolve, reject) => {
+      Axios.put(Config.api + route, params).then((res) => {
+        console.log("API PUT", route, params, res.data);
+        resolve(res);
+      }).catch((err) => {
+        console.log("ERROR: API PUT", route, params, err);
+      });
     });
-  });
+  }
+
+  Api.delete = (route, params) => {
+    return new Promise((resolve, reject) => {
+      Axios.delete(Config.api + route, params).then((res) => {
+        console.log("API DELETE", route, params, res.data);
+      }).catch((err) => {
+        console.log("ERROR: API DELETE", route, params, err);
+        reject(err);
+      });
+    });
+  }
+
+} else {
+
+  Api.get = (route) => {
+    return Axios.get(Config.api + route);
+  }
+
+  Api.post = (route, params) => {
+    return Axios.post(Config.api + route, params);
+  }
+
+  Api.put = (route, params) => {
+    return Axios.put(Config.api + route, params);
+  }
+
+  Api.delete = (route, params) => {
+    return Axios.delete(Config.api + route, params);
+  }
+
 }
--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,6 +1,7 @@
 const Config = {
   gauthClientId: "869104134693-s5s8j8qols4ggc8p26chv80vfnm6eqmm.apps.googleusercontent.com",
-  api: "http://localhost:3000/api"
+  api: "http://localhost:3000/api",
+  enableApiLogs: true, // should not be changed in runtime
 }
 
 export default Config;
