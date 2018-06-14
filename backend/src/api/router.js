2a886ddc99f9a9d6c751a1169a7eb668c2b670a9
Victor Breder <vbbreder@gmail.com>
2018-06-14 05:35:46 -0300

    cleanup

--- backend/src/api/router.js
+++ backend/src/api/router.js
@@ -1,19 +1,10 @@
 import Express from "express";
-import Multer from "multer";
-import FileSystem from "fs";
-import Path from "path";
 
 import AssignmentsRouter from "./assignments/router";
 import CoursesRouter from "./courses/router";
 import UsersRouter from "./users/router";
 import SubmissionsRouter from "./submissions/router";
 
-import CoursesModel from "./courses/model";
-import UsersModel from "./users/model";
-
-import { UPLOADS_DIR } from "../config";
-
-const upload = Multer();
 const router = Express.Router();
 
 router.use("/assignments", AssignmentsRouter);
