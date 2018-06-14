9299e3ecdbe5592c602f55e1d67021f0c144c538
Victor Breder <vbbreder@gmail.com>
2018-06-13 21:49:45 -0300

    api request logging

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -4,22 +4,51 @@ import Config from "./Config";
 
 export default class Api {};
 
+// @victorbreder: this is a wrapper around the Axios library
+// so we can log all Api calls and responses.
+
 Api.get = (route) => {
-  console.log("API GET", route);
-  return Axios.get(Config.api + route);
+  return new Promise((resolve, reject) => {
+    Axios.get(Config.api + route).then((res) => {
+      console.log("API GET", route, res.data);
+      resolve(res);
+    }).catch((err) => {
+      console.log("ERROR: API GET", route, err);
+      reject(err);
+    });
+  });
 }
 
 Api.post = (route, params) => {
-  console.log("API POST", route, params);
-  return Axios.post(Config.api + route, params);
+  return new Promise((resolve, reject) => {
+    Axios.post(Config.api + route).then((res) => {
+      console.log("API POST", route, params, res.data);
+      resolve(res);
+    }).catch((err) => {
+      console.log("ERROR: API POST", route, params, err);
+      reject(err);
+    });
+  });
 }
 
 Api.put = (route, params) => {
-  console.log("API PUT", route, params);
-  return Axios.put(Config.api + route, params);
+  return new Promise((resolve, reject) => {
+    Axios.put(Config.api + route).then((res) => {
+      console.log("API PUT", route, params, res.data);
+      resolve(res);
+    }).catch((err) => {
+      console.log("ERROR: API PUT", route, params, err);
+    });
+  });
 }
 
 Api.delete = (route, params) => {
-  console.log("API DELETE", route, params);
-  return Axios.delete(Config.api + route, params);
+  return new Promise((resolve, reject) => {
+    Axios.delete(Config.api + route).then((res) => {
+      console.log("API DELETE", route, params, res.data);
+    }).catch((err) => {
+      console.log("ERROR: API DELETE", route, params, err);
+      reject(err);
+    });
+  });
 }
