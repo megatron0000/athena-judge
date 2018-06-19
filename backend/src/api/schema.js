5b6617dfee2cd3ddee351b4146e594627193bf8a
Victor Breder <vbbreder@gmail.com>
2018-06-19 20:38:21 -0300

    added mock data so testing is faster

--- backend/src/api/schema.js
+++ backend/src/api/schema.js
@@ -20,4 +20,69 @@ export default async function sync() {
   await CoursesAssocModel.sync({ force: true });
   await UsersModel.sync({ force: true });
   console.log("Database schema synced");
+
+  // MOCK DATA
+  // Users generated at https://randomuser.me/
+  UsersModel.create({
+    gid: "1",
+    name: "Professor Paul",
+    photo: "https://randomuser.me/api/portraits/men/3.jpg",
+    email: "professor.paul@example.com",
+  });
+  UsersModel.create({
+    gid: "2",
+    name: "Student Steve",
+    photo: "https://randomuser.me/api/portraits/men/1.jpg",
+    email: "student.steve@example.com",
+  });
+  UsersModel.create({
+    gid: "3",
+    name: "Student Stephany",
+    photo: "https://randomuser.me/api/portraits/women/1.jpg",
+    email: "student.stephany@example.com",
+  });
+  UsersModel.create({
+    gid: "117441890382622508131",
+    name: "Student Placeholder",
+    photo: "https://randomuser.me/api/portraits/men/2.jpg",
+    email: "student.placeholder@example.com",
+  });
+
+  CoursesModel.create({
+    creatorUserGid: "1",
+    name: "CS-101",
+    description: "Introduction to Computer Science",
+  });
+
+  AssignmentsModel.create({
+    courseId: 1,
+    title: "Lab 1 - Printf",
+    description: "Create a program that prints a message to the user.",
+  });
+  AssignmentsModel.create({
+    courseId: 1,
+    title: "Lab 2 - Temperature Conversor",
+    description: "Create a program that converts temperature between the Celsus and Fahrenheit scales.",
+  });
+
+  CoursesAssocModel.create({
+    courseId: 1,
+    userGid: "1",
+    role: "professor",
+  });
+  CoursesAssocModel.create({
+    courseId: 1,
+    userGid: "2",
+    role: "student",
+  });
+  CoursesAssocModel.create({
+    courseId: 1,
+    userGid: "3",
+    role: "student",
+  });
+  CoursesAssocModel.create({
+    courseId: 1,
+    userGid: "117441890382622508131",
+    role: "student",
+  });
 }
