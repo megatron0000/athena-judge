67a0a12694ac6bb1377283ca23cd749f1a8726de
Ítalo Tabatinga <italotabatinga@gmail.com>
2018-06-18 21:36:36 -0300

    Fixing code quality issues

--- frontend/src/SideBar/CoursesList.js
+++ frontend/src/SideBar/CoursesList.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 
