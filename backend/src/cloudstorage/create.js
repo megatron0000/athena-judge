a2cc84cae296d59bf82036ce0bc1d1dcee19b24d
igor-ribeiiro <igormr98mr@gmail.com>
2019-04-06 14:25:46 -0300

    Adding .json

---
+++ backend/src/cloudstorage/create.js
@@ -0,0 +1,20 @@
+  // Imports the Google Cloud client library
+  const {Storage} = require('@google-cloud/storage');
+ 
+  // Creates a client
+  const storage = new Storage({
+    projectId: 'ces29-athena',
+    keyFilename: '../credentials/keyfile.json'
+  }); 
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   */
+  const bucketName = 'bucket-name-athena-test';
+ 
+  async function createBucket() {
+    // Creates the new bucket
+    await storage.createBucket(bucketName);
+    console.log(`Bucket ${bucketName} created.`);
+  }
+ 
+  createBucket();
