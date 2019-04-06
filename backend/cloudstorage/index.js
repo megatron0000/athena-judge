23f52a6c25934bb5531782fe6ca47b4b6dba3d8e
igor-ribeiiro <igormr98mr@gmail.com>
2019-04-06 16:05:17 -0300

    Working now directory paths

--- backend/cloudstorage/index.js
+++ backend/cloudstorage/index.js
@@ -2,15 +2,22 @@ const GCS = require('./lib.js'); // GCS = Google Cloud Storage
 const fs = require('fs');
 const path = require('path');
 
+const getFiles = (dir) => {
+    const files = allFilesSync(dir);
+
+}
+
 const allFilesSync = (dir, fileList = []) => {
     fs.readdirSync(dir).forEach(file => {
         const filePath = path.join(dir, file)
-
-        fileList.push(
-            fs.statSync(filePath).isDirectory()
-                ? {[file]: allFilesSync(filePath)}
-                : file
-        )
+        
+        if(fs.statSync(filePath).isDirectory()) {
+            allFilesSync(filePath).forEach(children => {
+                fileList.push(file + '/' + children);
+            });
+        } else {
+            fileList.push(file);
+        }
     })
     return fileList
 }
@@ -18,8 +25,22 @@ const allFilesSync = (dir, fileList = []) => {
 async function submitStudentDirectory(directory) {
     filesList = allFilesSync(directory);
     console.log(filesList);
-    // GCS.uploadFile(directory, "destinationPath");
-    // GCS.listFiles();
+
+    // filesList.forEach(file => {
+    //     let directory = '';
+    //     if(file instanceof Map) {
+    //         directory += file;
+    //         const dir = file;
+    //         dir.forEach(file => {
+                
+    //         });
+    //     }
+    //     const filePath = path.join(directory, file);
+    //     console.log("FilePath = ", filePath);
+    //     GCS.uploadFile(filePath, filePath);
+    // });
+
+    GCS.listFiles();
 }
 
 
