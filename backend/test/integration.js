a9a2ea2f2843ad567fadde472d829d290687c86c
Felipe <felipecoimbra97@gmail.com>
2019-04-07 20:37:00 -0300

    [WIP] Initial integration

--- backend/test/integration.js
+++ backend/test/integration.js
@@ -1,13 +1,24 @@
 require('../credentials/config')
 
-const assert = require('assert')
+const { Authenticate } = require('../credentials/auth')
+const { AttachPubSubListener, StopPubSub } = require('../pubsub')
+const { classroom_v1 } = require('googleapis')
 
 describe('Integration', function () {
-
   this.timeout(60000)
 
-  it('should do something', async () => {
-    
-  })
+  before(async () => {
+    const auth = await Authenticate();
+    this.classroomClient = new classroom_v1.Classroom({ auth });
+
+    this.COURSE_ID = 31645086781;  // CES_TESTE course
+    this.COURSEWORK_ID = 36000414864; // "teste novo" course work
+  });
+
+  it('should listen to CourseWork publish events and retrieve test files from GCS', async () => {
+      AttachPubSubListener(async (pubsubMessage) => {
+        console.log(pubsubMessage);
+      });
+  });
 
 })
