f09201e8f9b3bdf1ef730d8d2c6461244eca310f
Felipe <felipecoimbra97@gmail.com>
2019-04-07 18:55:15 -0300

    Bootstrap on integration test

---
+++ backend/test/integration.js
@@ -0,0 +1,13 @@
+require('../credentials/config')
+
+const assert = require('assert')
+
+describe('Integration', function () {
+
+  this.timeout(60000)
+
+  it('should do something', async () => {
+    
+  })
+
+})
